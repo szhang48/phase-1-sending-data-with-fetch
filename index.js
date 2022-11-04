6e7796ce9f150829ad46854299c1baca90cc7fa5
Sandy Zhang <sandyzhang142@gmail.com>
2022-11-04 12:13:25 -0400

    commit tree e9362f9f38652a1b79e2efbe2d0409afd15a4d9f

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData(name,email){ 
+    // const form = document.querySelector('#submit')
+    // form.addEventListener('submit', (e) => {
+    //     e.preventDefault()
+    //     const name = e.target['name'].value
+    //     const email = e.target['email'].value
+      
+       return fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body : JSON.stringify({
+                name,
+                email,
+                
+            }),
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            document.body.innerHTML = json.id
+        })
+        .catch(function (error) {
+            document.body.innerHTML = error.message
+            console.log(error.message)
+        })
+//    })
+}
+// submitData();
+
+
